9acfe463ec83e9de16c4ce1d52398448266d21a7
vattigunta siva sankar reddy <138890207+sivasankarreddyvattigunta@users.noreply.github.com>
2023-09-10 08:50:21 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,54 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+def modulo(x, y):
+    if y == 0:
+        return "Cannot calculate remainder with zero!"
+    return x % y
+
+def power(x, y):
+    return x ** y
+
+while True:
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'modulo' for remainder calculation")
+    print("Enter 'power' for exponentiation")
+    print("Enter 'quit' to end the program")
+    
+    user_input = input(": ")
+    
+    if user_input == "quit":
+        break
+    elif user_input in ("add", "subtract", "multiply", "divide", "modulo", "power"):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        
+        if user_input == "add":
+            print("Result: ", add(num1, num2))
+        elif user_input == "subtract":
+            print("Result: ", subtract(num1, num2))
+        elif user_input == "multiply":
+            print("Result: ", multiply(num1, num2))
+        elif user_input == "divide":
+            print("Result: ", divide(num1, num2))
+        elif user_input == "modulo":
+            print("Result: ", modulo(num1, num2))
+        elif user_input == "power":
+            print("Result: ", power(num1, num2))
+    else:
+        print("Invalid input. Please enter a valid operation.")
